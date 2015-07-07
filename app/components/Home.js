fc2d6235a268ca29210d7613c186128b1bd21643
Thomas Maximini <tmaximini@gmail.com>
2015-07-07 10:30:21 +0200

    init ¯\_(ツ)_/¯

---
+++ app/components/Home.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Home = React.createClass({
+
+    render: function() {
+        return (
+            <h2 className="text-center">
+                Search by Github username above
+            </h2>
+        )
+    }
+
+});
+
+module.exports = Home;
+
